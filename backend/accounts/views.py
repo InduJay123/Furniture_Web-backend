17dea70a9a4bac0a47de63d8b967aa8f5c4ed078
Induni Jayamanna <152453002+InduJay123@users.noreply.github.com>
2026-01-17 17:42:47 +0530

    auth update 2

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -11,6 +11,7 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import status
 
 class RegisterView(generics.CreateAPIView):
+    permission_classes = [AllowAny]
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
     permission_classes = [AllowAny]
