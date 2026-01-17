bb79b0bb6192f771a654eb8306e664d54d5571c3
Induni Jayamanna <152453002+InduJay123@users.noreply.github.com>
2026-01-17 16:58:16 +0530

    auth with email and password

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,12 +1,8 @@
+# accounts/urls.py
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-from .views import RegisterView
+from .views import RegisterView, EmailLoginView 
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', TokenObtainPairView.as_view(), name='login'),
-    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("register/", RegisterView.as_view(), name="register"),
+    path("login/", EmailLoginView.as_view(), name="login"),
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -2,7 +2,37 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 from django.contrib.auth.models import User
 from .serializers import RegisterSerializer
+from rest_framework.permissions import AllowAny
+
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import status
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+
+
+class EmailLoginView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        email = request.data.get("email")
+        password = request.data.get("password")
+        try:
+            user_obj = User.objects.get(email=email)
+        except User.DoesNotExist:
+            return Response({"detail": "No active account found with the given credentials"}, status=401)
+
+        user = authenticate(username=user_obj.username, password=password)
+        if user:
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                "refresh": str(refresh),
+                "access": str(refresh.access_token)
+            })
+
+        return Response({"detail": "No active account found with the given credentials"}, status=401)
