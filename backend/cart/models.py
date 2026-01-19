17678b09c6ad0cace7275677cad110e85889390c
Induni Jayamanna <152453002+InduJay123@users.noreply.github.com>
2026-01-19 21:51:59 +0530

    cart model update

--- backend/cart/models.py
+++ backend/cart/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from djnago.contrib.auth.model import User
+from products.models import Product
 # Create your models here.
+class Cart(model.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Cart"
