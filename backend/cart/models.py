9b93bcd763f8128e07924668bba1b509e0c0b600
Induni Jayamanna <152453002+InduJay123@users.noreply.github.com>
2026-01-19 21:56:41 +0530

    cart item model update

--- backend/cart/models.py
+++ backend/cart/models.py
@@ -8,3 +8,14 @@ class Cart(model.Model):
 
     def __str__(self):
         return f"{self.user.username}'s Cart"
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    class Meta:
+        unique_together = ('cart', 'product')
+        
+    def __str__(self):
+        return f"{self.quantity} of {self.product.name} in {self.cart.user.username}'s Cart"
