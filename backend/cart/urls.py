99e7b044ee75e266156db2c73030b371eacff76a
Induni Jayamanna <152453002+InduJay123@users.noreply.github.com>
2026-01-22 00:11:52 +0530

    cart url update

--- backend/cart/urls.py
+++ backend/cart/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import add_to_cart, view_cart
+from .views import add_to_cart, view_cart, update_cart_item,remove_cart_item
 
 urlpatterns = [
     path("add/", add_to_cart),
     path("view/", view_cart),
+    path("update/", update_cart_item),
+    path("remove/", remove_cart_item),
 ]
