1de0b6a378c93c0e055e51b8b60c02f59ced55ff
Induni Jayamanna <152453002+InduJay123@users.noreply.github.com>
2026-01-26 11:42:40 +0530

    url update

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('api/products/', include('products.urls')),
     path('api/accounts/', include('accounts.urls')),
     path("api/cart/", include("cart.urls")),
+    path("api/orders/", include("orders.urls")),
 ]
