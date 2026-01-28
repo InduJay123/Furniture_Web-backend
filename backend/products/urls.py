1c0ad81604584395312ad3d74c7599725c9437e4
Induni Jayamanna <152453002+InduJay123@users.noreply.github.com>
2026-01-28 21:57:56 +0530

    database update

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import ProductCreateView, ProductListView
+from .views import ProductCreateView, ProductListView, ProductDetailView, ProductUpdateDeleteView
 
 urlpatterns = [
     path('add/', ProductCreateView.as_view()),
     path('list/', ProductListView.as_view()),
+    
+    path("<int:pk>/", ProductDetailView.as_view()),
+    path("<int:pk>/update/", ProductUpdateDeleteView.as_view()),
+    path("<int:pk>/delete/", ProductUpdateDeleteView.as_view()),
 ]
