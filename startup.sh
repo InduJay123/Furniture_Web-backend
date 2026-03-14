f2c16d2f1255590a6f2c4e57dd12cadbe5cb2a8f
Induni Jayamanna <152453002+InduJay123@users.noreply.github.com>
2026-03-15 01:14:24 +0530

    bugfix 3

--- startup.sh
+++ startup.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# Activate virtual environment
-source /home/site/wwwroot/venv/bin/activate
+# Go to the Django project folder
+cd /home/site/wwwroot/backend
 
-# Install requirements
-pip install -r requirements.txt
-python backend/manage.py migrate
-python backend/manage.py collectstatic --noinput
-gunicorn backend.backend.wsgi --bind 0.0.0.0:$PORT
+# Install requirements using system Python
+pip install -r ../requirements.txt
+
+# Apply migrations
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Start Gunicorn
+gunicorn backend.wsgi:application --bind 0.0.0.0:$PORT
