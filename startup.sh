99467d3d5864a456d0836eadd284f7146f45a806
Induni Jayamanna <152453002+InduJay123@users.noreply.github.com>
2026-03-14 22:54:35 +0530

    error correction

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 
 # Activate virtual environment if you have one
 # source venv/bin/activate   <-- uncomment if you have a venv
-
+pip install -r requirements.txt
 python backend/manage.py migrate
 python backend/manage.py collectstatic --noinput
 gunicorn backend.backend.wsgi --bind 0.0.0.0:$PORT
