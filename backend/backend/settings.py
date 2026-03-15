9bfdbe25bb580fefc6c7f498b01fe1be5bcea7c0
Induni Jayamanna <152453002+InduJay123@users.noreply.github.com>
2026-03-15 15:16:23 +0530

    debug

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1"
 ]
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Application definition
 
@@ -89,7 +90,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'furniture_db',
-        'USER': 'indunijayamanna',
+        'USER': 'indunijayamanna@myshopdb1',
         'PASSWORD': 'Test@123456',
         'HOST': 'myshopdb1.mysql.database.azure.com',
         'PORT': '3306',
@@ -151,7 +152,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
